768453b09e004ec3b51a3d074ee5fc1772ef5962
Alireza Manizani <53353567+aligence@users.noreply.github.com>
2024-01-17 21:05:14 -0800

    Video Works, gonna download tailwind

---
+++ DrakeProject/src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className = "pageView">
+        <div id = "home-hero">
+          <div className='home-hero-logo'>
+            <img src='../../ovo.png'></img>
+          </div>
+          <video loop autoPlay playsInline muted>
+            <source src='../../drake.mp4' type='video/mp4'></source>
+            Your browser doesnt support this
+          </video>
+      
+          <h1>Vite + React</h1>
+          <div className="card">
+            
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ DrakeProject/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Navbar(){
+    (
+    <div>
+        
+    </div>
+    )
+}
+export default Navbar

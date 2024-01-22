07e40e9857012ff633f1aff99dfef5a8cb22a45e
Alireza Manizani <53353567+aligence@users.noreply.github.com>
2024-01-22 15:24:57 -0800

    Still figuring out what i want it to look like

--- DrakeProject/src/App.tsx
+++ DrakeProject/src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css'
-
+import Navbar from './components/Navbar'
 function App() {
   return (
     <>
       <div className = "pageView">
         <div id = "home-hero">
-          <div className='home-hero-logo'>
+        <div className='home-hero-logo'>
             <img src='../../ovo.png'></img>
-          </div>
+        </div>
+          <Navbar></Navbar>
           <video loop autoPlay playsInline muted>
             <source src='../../drake.mp4' type='video/mp4'></source>
             Your browser doesnt support this
           </video>
       
           <h1>Vite + React</h1>
+        
           <div className="card">
             
             <p>
--- DrakeProject/src/components/Navbar.tsx
+++ DrakeProject/src/components/Navbar.tsx
@@ -1,9 +1,10 @@
-import React from "react";
 function Navbar(){
-    (
-    <div>
-        
-    </div>
+    return (
+    <nav className="flex items-center justify-between px-4 w-1/3 rounded-2xl bg-gray-200 backdrop-blur-xl bg-opacity-10 h-16 absolute z-10 top-1 right-1 text-3xl text-white font-bold">
+        <a href="#" className="text-white"> [ HOME ]</a> 
+        <a href="#" className="text-white">[ ABOUT ]</a> 
+        <a href="#" className="text-white">[ LINKS ]</a> 
+    </nav>
     )
 }
 export default Navbar

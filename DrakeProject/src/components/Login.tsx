6a1c60772d47b8c43d996d88bdc552717a9a4e9c
Alireza Manizani <53353567+aligence@users.noreply.github.com>
2024-02-08 00:14:58 -0800

    screw that one vid, im just gonna get the code and work with it

--- DrakeProject/src/components/Login.tsx
+++ DrakeProject/src/components/Login.tsx
@@ -1,10 +1,11 @@
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=e1635bb6cf544f28a1aa7c4844000ff7&response_type=code&redirect_uri=http://localhost:5173/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 function Login(){
     return (
-        <div>
-                <a className="btn btn-success btn-lg" href = {AUTH_URL}>
-                    Login With Spotify
-                </a>
+        <div className="bg-black h-full">
+            <h1 className="pb-50">To Get More Information, Please</h1>
+            <a className="btn btn-lg" href = {AUTH_URL}>
+                Login With Spotify
+            </a>
         </div>
     )
 }
